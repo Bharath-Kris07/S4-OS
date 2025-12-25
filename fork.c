5b9b2169d45e6ac231714193003b34961a4aa432
Bharath-Kris07 <phantomkrisx@gmail.com>
2025-12-25 14:05:06 +0000

    Initial commit of OS lab code

---
+++ fork.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+int main(){
+    pid_t t;
+    t=fork();
+    if(t<0){
+        fprintf(stderr,"fork failed");
+        return 1;
+    }
+    else if(t==0){
+        puts("inside child process");
+        execlp("/bin/ls","ls",NULL);
+    }
+    else{
+        puts("inside parent process");
+        wait(NULL);
+        puts("child complete");
+        return 0;
+    }
+}
