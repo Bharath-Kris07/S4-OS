e35fab75caae6511231885d124b9490b4ceadf02
Bharath Krishna <phantomkrisx@gmail.com>
2025-12-26 22:42:01 +0530

    Add initial implementation of pstree.c for process creation demonstration

---
+++ pstree.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+int main() {
+    pid_t pid;
+    pid = fork();
+    if (pid < 0) {
+        fprintf(stderr, "Fork failed\n");
+        return 1;
+    }
+    else if (pid == 0) {
+        // getpid() = My ID, getppid() = Parent's ID
+        printf("Child Process:  PID = %d (My Parent is %d)\n", getpid(), getppid());
+        sleep(80); 
+    }
+    else {
+        printf("Parent Process: PID = %d (I created Child %d)\n", getpid(), pid);
+        sleep(80);
+    }
+    return 0;
+}
