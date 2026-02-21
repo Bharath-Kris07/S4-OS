feacc823f724d9deb28645a8e7e664018f0893bb
Bharath Krishna <phantomkrisx@gmail.com>
2026-02-21 19:29:34 +0530

    fixed formatting in pstree.c

--- pstree.c
+++ pstree.c
@@ -4,11 +4,11 @@
 int main() {
     pid_t pid;
     pid = fork();
-    if (pid < 0) {
+    if (pid<0) {
         fprintf(stderr, "Fork failed\n");
         return 1;
     }
-    else if (pid == 0) {
+    else if (pid==0) {
         // getpid() = My ID, getppid() = Parent's ID
         printf("Child Process:  PID = %d (My Parent is %d)\n", getpid(), getppid());
         sleep(80); 
